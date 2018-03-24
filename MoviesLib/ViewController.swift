d07c1997d9b3ed12fb29a3cd1585fc301ae11191
Usuário Convidado <Guest@pa06micro41.local>
2018-03-24 16:57:28 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 24/03/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hide(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 3.0) {
+            sender.isHidden = true
+        }
+        
+        
+    }
+    
+
+}
